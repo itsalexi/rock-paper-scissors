29be754127fad67792c626bfd2fd410fb62a70f8
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 09:37:59 +0800

    Fixed the computerPlay function

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-function computerPlay(){
-    random = Math.floor(Math.random()*3);
-    
-    switch (random) {
-        case 1:
-            return "Rock"
-            break;
-        case 2:
-            return "Paper"
-            break;
-        case 3:
-            return "Scissors" 
-            break;
-    }
+function computerPlay() {
+  random = Math.floor(Math.random() * 3) + 1;
+  switch (random) {
+    case 1:
+      return "Rock";
+      break;
+    case 2:
+      return "Paper";
+      break;
+    case 3:
+      return "Scissors";
+      break;
+  }
 }
 
-console.log(computerPlay())
+console.log(computerPlay());
