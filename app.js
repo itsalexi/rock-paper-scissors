c48c4296cfd9dac1fa1ed0eb401df8b5c3f28c13
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 09:32:14 +0800

    Created a computerPlay function

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function computerPlay {
+    random = Math.floor(Math.random()*3);
     
+    switch (random) {
+        case 1:
+            return "rock"
+            break;
+        case 2:
+            return "paper"
+            break;
+        case 3:
+            return "scissors" 
+            break;
+    }
 }
